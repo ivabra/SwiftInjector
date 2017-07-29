8cb7edeca5c8e3883d9ed31a33c061d4d7ca1056
Ivan Brazhnikov <samsungpc239@gmail.com>
2017-07-29 23:16:19 +0300

    Initial Commit

---
+++ SwiftInjector/SwiftInjector.h
@@ -0,0 +1,19 @@
+//
+//  SwiftInjector.h
+//  SwiftInjector
+//
+//  Created by Ivan Brazhnikov on 29/07/2017.
+//  Copyright © 2017 Ivan Brazhnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftInjector.
+FOUNDATION_EXPORT double SwiftInjectorVersionNumber;
+
+//! Project version string for SwiftInjector.
+FOUNDATION_EXPORT const unsigned char SwiftInjectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftInjector/PublicHeader.h>
+
+
